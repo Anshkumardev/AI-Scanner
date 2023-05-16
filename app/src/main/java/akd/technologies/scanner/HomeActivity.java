adda925a11b71e91bac77390f27c1cdc14ebc42c
Anshkumardev <anshkumardev@gmail.com>
2023-05-16 21:29:52 +0530

    Image Picker Updated

--- app/src/main/java/akd/technologies/scanner/HomeActivity.java
+++ app/src/main/java/akd/technologies/scanner/HomeActivity.java
@@ -1,13 +1,27 @@
 package akd.technologies.scanner;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Dialog;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.scanner.R;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
@@ -22,11 +36,24 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private static final int CAMERA_REQUEST_CODE = 200;
+    private static final int STORAGE_REQUEST_CODE = 400;
+    private static final int IMAGE_PICK_GALLERY_CODE = 1000;
+    private static final int IMAGE_PICK_CAMERA_CODE = 1001;
+
+    ImageView view_image;
+
+    String cameraPermission[];
+    String storagePermission[];
+
+    Uri image_uri;
+
     TextView username,email;
     Button signOut;
 
     CardView translate_and_summarize,translate;
 
+    ImageView camera_image,gallery_image;
 
     FirebaseAuth auth;
     FirebaseDatabase database;
@@ -39,19 +66,64 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        view_image = findViewById(R.id.image_view_block);
+
         username = findViewById(R.id.guserName);
         //email = findViewById(R.id.gemail);
         signOut = findViewById(R.id.signOutButton);
 
+        Dialog dialog = new Dialog(HomeActivity.this);
+
         translate_and_summarize = findViewById(R.id.translate_summarize);
         translate = findViewById(R.id.translate);
+
+        cameraPermission = new String[]{Manifest.permission.CAMERA,Manifest.permission.WRITE_EXTERNAL_STORAGE};
+
+        storagePermission = new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE};
 
         //------------------------Redirect to Translate and summarize page--------------------//
 
         translate_and_summarize.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //do something
+
+                dialog.setContentView(R.layout.image_selection_dialog);
+                dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                dialog.setCancelable(true);
+
+                camera_image = dialog.findViewById(R.id.camera_selection);
+                gallery_image = dialog.findViewById(R.id.gallery_selection);
+
+                camera_image.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        if(!checkCameraPermission()){
+                            requestCameraPermission();
+                        }
+                        else{
+                            pickCamera();
+                            dialog.dismiss();
+                        }
+                    }
+                });
+
+                gallery_image.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if(!checkStoragePermission()){
+
+                            requestStoragePermission();
+                        }
+                        else {
+                            pickGallery();
+                            dialog.dismiss();
+                        }
+                    }
+                });
+
+                dialog.show();
+
             }
         });
 
@@ -88,6 +160,41 @@ public class HomeActivity extends AppCompatActivity {
 
     }
 
+    private void pickGallery() {
+
+
+        Intent intent = new Intent(Intent.ACTION_PICK,MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        intent.setType("image/*");
+        intent.putExtra("return-data", true);
+        startActivityForResult(intent,IMAGE_PICK_GALLERY_CODE);
+    }
+
+    private void pickCamera() {
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE,"New AI Scanner Pictre");
+        values.put(MediaStore.Images.Media.DESCRIPTION,"Image Translation");
+        image_uri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,values);
+
+        Intent CameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        CameraIntent.putExtra(MediaStore.EXTRA_OUTPUT,image_uri);
+        startActivityForResult(CameraIntent,IMAGE_PICK_CAMERA_CODE);
+    }
+
+    private void requestStoragePermission() {
+        ActivityCompat.requestPermissions(this,storagePermission,STORAGE_REQUEST_CODE);
+
+    }
+
+    private boolean checkStoragePermission() {
+        boolean result = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == (PackageManager.PERMISSION_GRANTED);
+        return result;
+    }
+
+    private void requestCameraPermission() {
+
+        ActivityCompat.requestPermissions(this,cameraPermission,CAMERA_REQUEST_CODE);
+    }
+
     void signout(){
         GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
         FirebaseUser user = auth.getCurrentUser();
@@ -107,5 +214,66 @@ public class HomeActivity extends AppCompatActivity {
             startActivity(new Intent(HomeActivity.this,MainActivity.class));
         }
 
+    }
+
+    //--------------------check camera permission--------------------------//
+
+    private boolean checkCameraPermission(){
+        boolean result = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)  ==  (PackageManager.PERMISSION_GRANTED);
+        boolean result1 = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == (PackageManager.PERMISSION_GRANTED);
+        return result1 && result;
+    }
+
+    //--------------------------Handling Permission Result---------------------//
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case CAMERA_REQUEST_CODE:
+                if (grantResults.length > 0) {
+                    boolean cameraAccepted = grantResults[0] ==
+                            PackageManager.PERMISSION_GRANTED;
+                    boolean writeStorageAccepted = grantResults[0] ==
+                            PackageManager.PERMISSION_GRANTED;
+                    if (cameraAccepted && writeStorageAccepted) {
+                        pickCamera();
+                    } else {
+                        Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                break;
+            case STORAGE_REQUEST_CODE:
+
+                if (grantResults.length > 0) {
+                    boolean writeStorageAccepted = grantResults[0] ==
+                            PackageManager.PERMISSION_GRANTED;
+                    if (writeStorageAccepted) {
+                        pickGallery();
+                    } else {
+                        Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                break;
+        }
+    }
+
+    //-----------------------------Handling Image Result-----------------------//
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == IMAGE_PICK_GALLERY_CODE) {
+                image_uri = data.getData();
+
+            }
+
+
+            view_image.setImageURI(image_uri);
+
+        }
     }
 }
